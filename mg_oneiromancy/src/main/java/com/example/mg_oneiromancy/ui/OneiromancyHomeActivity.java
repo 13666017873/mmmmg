58d201965090fb8cd9bedf6b102d5be9d94553dd
mmmmg <2291918627@qq.com>
2018-12-14 15:51:08 +0800

    merge dev

---
+++ mg_oneiromancy/src/main/java/com/example/mg_oneiromancy/ui/OneiromancyHomeActivity.java
@@ -0,0 +1,22 @@
+package com.example.mg_oneiromancy.ui;
+
+import com.example.mg_oneiromancy.R;
+import com.example.resource.tools_kit.BaseActivity;
+
+public class OneiromancyHomeActivity extends BaseActivity {
+
+    @Override
+    public int layout() {
+        return R.layout.oneiromancy_home_activity;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initDate() {
+
+    }
+}

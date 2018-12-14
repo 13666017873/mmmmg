0be22e8edb78ffa2661212515e4169ccc5a4df51
mmmmg <2291918627@qq.com>
2018-12-14 15:40:37 +0800

    添加解梦几æâ口

--- common/src/main/java/com/example/resource/ui/MainActivity.java
+++ common/src/main/java/com/example/resource/ui/MainActivity.java
@@ -1,43 +1,23 @@
 package com.example.resource.ui;
 
-import android.os.Bundle;
-import android.widget.Button;
-
-import com.app.hubert.guide.NewbieGuide;
-import com.app.hubert.guide.model.GuidePage;
-import com.app.hubert.guide.model.HighLight;
 import com.example.resource.R;
-import com.example.resource.commenView.CommentTimePickerView;
-import com.example.resource.utils.NumberType;
+import com.example.resource.tools_kit.BaseActivity;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public int layout() { return R.layout.activity_main; }
 
-        setContentView(R.layout.activity_main);
+    @Override
+    public void initView() {
+        RecyclerView recyclerView = findViewById(R.id.activity_main_recyclerView);
 
-        Button button = findViewById(R.id.btn);
+    }
 
-        NewbieGuide.with(this)
-                .setLabel("guide1")
-                .addGuidePage(GuidePage.newInstance()
-//                        .setBackgroundColor(getResources().getColor(R.color.bg))
-                        .addHighLight(button, HighLight.Shape.ROUND_RECTANGLE,25)
-                        .setLayoutRes(R.layout.view_guide_simple))
-                .show();
+    @Override
+    public void initDate() {
 
-
-        CommentTimePickerView commentTimePickerView = findViewById(R.id.comment_time_picker_view);
-
-        commentTimePickerView.setOnListener(new CommentTimePickerView.onListener() {
-            @Override
-            public void onChangeListener(NumberType numberType, String pickValue, String oldValue, String newValue) {
-
-            }
-        });
     }
 }
--- common/src/main/java/com/example/resource/ui/TwoActivity.java
+++ common/src/main/java/com/example/resource/ui/TwoActivity.java
@@ -13,5 +13,26 @@ public class TwoActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two);
+
+//        Button button = findViewById(R.id.btn);
+//
+//        NewbieGuide.with(this)
+//                .setLabel("guide1")
+//                .addGuidePage(GuidePage.newInstance()
+////                        .setBackgroundColor(getResources().getColor(R.color.bg))
+//                        .addHighLight(button, HighLight.Shape.ROUND_RECTANGLE,25)
+//                        .setLayoutRes(R.layout.view_guide_simple))
+//                .show();
+//
+//
+//        CommentTimePickerView commentTimePickerView = findViewById(R.id.comment_time_picker_view);
+//
+//        commentTimePickerView.setOnListener(new CommentTimePickerView.onListener() {
+//            @Override
+//            public void onChangeListener(NumberType numberType, String pickValue, String oldValue, String newValue) {
+//
+//            }
+//        });
+
     }
 }
